759f0fc0e56529506617a335ff18c6102387d55c
Rafcyk <rafcy9@gmail.com>
2020-04-20 13:04:41 +0200

    Updates

---
+++ precache-manifest.9f4d594cbc791fad52d764c08ffbda5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4874b023e8268de41cbf64c71bd46a4",
+    "url": "/PZ2_2020_GR02/web/index.html"
+  },
+  {
+    "revision": "c464476245b1a63552bc",
+    "url": "/PZ2_2020_GR02/web/static/css/main.5a78c7fb.chunk.css"
+  },
+  {
+    "revision": "f7045c006b1b1ebf8e8d",
+    "url": "/PZ2_2020_GR02/web/static/js/2.a2dc2f5a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PZ2_2020_GR02/web/static/js/2.a2dc2f5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c464476245b1a63552bc",
+    "url": "/PZ2_2020_GR02/web/static/js/main.8fd44a08.chunk.js"
+  },
+  {
+    "revision": "e5e8d679facb4c370154",
+    "url": "/PZ2_2020_GR02/web/static/js/runtime-main.20e41cb9.js"
+  },
+  {
+    "revision": "16f0f6df56da6efa8a4ebc8fd83c5265",
+    "url": "/PZ2_2020_GR02/web/static/media/author.16f0f6df.png"
+  },
+  {
+    "revision": "371fcdc708904595c617616a89d73ebd",
+    "url": "/PZ2_2020_GR02/web/static/media/authorSecond.371fcdc7.png"
+  }
+]);
