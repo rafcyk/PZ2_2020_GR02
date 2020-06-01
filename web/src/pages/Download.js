e3c8301cc6802532c096b973c1a7fedb2524bc09
Rafcyk <rafcy9@gmail.com>
2020-06-01 15:13:14 +0200

    add links to games

--- web/src/pages/Download.js
+++ web/src/pages/Download.js
@@ -7,8 +7,8 @@ const Download = () => {
             <div className="wrapDownload">
                 <p>Wybierz swoją wersję</p>
                 <div className = 'wrapButtons'>
-                    <button className="mobile">Android</button>
-                    <button className="windows">Windows</button>
+                    <a className="mobile" href = "https://mega.nz/file/cuR1WShS#6FwHtgfrCN5Ui8w1c5rstGTGMbVj5fDb3II4_Mc4K0w" target = "_blank">Android</a>
+                    <a className="windows" href = "https://mega.nz/file/ZuIhCa5R#lEHNpYa-7mKSbcSMrdMS9Xut_g2Cj9XtIA4xeP75kok" target = "_blank">Windows</a>
                 </div>
             </div>
         </section>
