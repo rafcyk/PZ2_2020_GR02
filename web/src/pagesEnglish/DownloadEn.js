83174f6a9cd9dde67ad1a9bd84942db07a371a49
Rafcyk <rafcy9@gmail.com>
2020-06-08 14:38:43 +0200

    change android link

--- web/src/pagesEnglish/DownloadEn.js
+++ web/src/pagesEnglish/DownloadEn.js
@@ -8,7 +8,7 @@ const DownloadEn = () => {
                 <p>
                     Choose your version</p>
                 <div className='wrapButtons'>
-                    <a className="mobile" href = "https://mega.nz/file/cuR1WShS#6FwHtgfrCN5Ui8w1c5rstGTGMbVj5fDb3II4_Mc4K0w" target = "_blank">Android</a>
+                    <a className="mobile" href = "https://mega.nz/file/8mgT0BJI#bTWdCJcpiTclulFPnZj6rtDPWzbputPCRe-Y0XPMvAs" target = "_blank">Android</a>
                     <a className="windows" href = "https://mega.nz/file/ZuIhCa5R#lEHNpYa-7mKSbcSMrdMS9Xut_g2Cj9XtIA4xeP75kok" target = "_blank">Windows</a>
                 </div>
             </div>
